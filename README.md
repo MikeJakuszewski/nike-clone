27c8bfc93f7036c3f292756e35aa2b3297753d83
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-11-06 12:12:04 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Awesome Project
+
+## How It's Made:
+
+**Tech used:** React, Tailwind, Vite
+
+## Optimizations
+
+## Lessons Learned:
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className=" text-lime-500 text-xl bg-slate-500">Hello World</div>;
+}
+
+export default App;
