ee7078246dab4433c0c42be574b43e17c3a91f34
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-11-07 18:25:17 -0500

    [Add Feature] Completed subscribe and footer section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,4 +1,22 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      id="contact-us"
+      className="max-container flex justify-between items-center flex-col gap-10"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-full font-palanquin font-bold">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+      <div className=" w-full max-w-3xl xl:max-w-5xl flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" className="w-full" />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Subscribe;
